eaece8f6f674e0c0c0f33ab5cbac7b3c03a8e48c
Enoch Kim <enochkim91@gmail.com>
2017-09-28 17:09:02 -0700

    Completes longest consecutive sequence in O(n)

--- dumbledores-pensieve/longestConsecutiveSequence.js
+++ dumbledores-pensieve/longestConsecutiveSequence.js
@@ -8,38 +8,81 @@ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 Your algorithm should run in O(n) complexity.
 */
 
+// const longestConsecutiveSequence = (array) => {
+//   if (array.length === 0 || array === null) {
+//     return 0;
+//   }
+//   const set = new Set(array);
+//   let longestStreak = 1
+
+
+
+
+//   for (let i = 0; i < array.length; i++) {
+//     let currentStreak = 1
+//     currentNum = array[i] + 1
+//     while(set.has(currentNum)) {
+//       currentStreak++;
+//       currentNum++;
+//     }
+
+//     longestStreak = Math.max(longestStreak, currentStreak)
+//   }
+
+//   return longestStreak;
+
+// }
+
+
 const longestConsecutiveSequence = (array) => {
-  if (array.length === 0 || array === null) {
-    return 0;
+
+  if (array.length === 0) {
+    return 0
   }
-  const set = new Set(array);
-  let longestStreak = 1
+  // keep track of currentLength, longestLength and initiate a storage object
 
+  let longestLength = 1;
+  let currentLength = 1;
+  let storageObject = {}; // keep track of what numbers we have visited to keep it in O(n) time
 
-
-
+  //populate our storage array with input numbers
+  for(let i = 0; i < array.length; i++) {
+    storageObject[array[i]] = true;
+  }
+  // iterate through input array
   for (let i = 0; i < array.length; i++) {
-    let currentStreak = 1
-    currentNum = array[i] + 1
-    while(set.has(currentNum)) {
-      currentStreak++;
-      currentNum++;
+    let currentNumber = array[i];
+
+  // see if numbers below are present
+    while(storageObject[currentNumber - 1]) {
+    // if they are we want to mark it as already seen
+    // increment current length
+    currentLength++;
+    storageObject[currentNumber - 1] = false;
+    currentNumber--;
     }
 
-    longestStreak = Math.max(longestStreak, currentStreak)
+    // reset current Number so we can go above
+    currentNumber = array[i];
+  // see if numbers above are present
+    while(storageObject[currentNumber + 1]) {
+    // if they are - mark it as visited
+    // increment current length
+    currentLength++;
+    storageObject[currentNumber + 1] = false;
+    currentNumber++;
+    }
+    
+  // compare current Length with longest length
+    longestLength = Math.max(currentLength, longestLength);
+    // reset current length to one for next iteration
+    currentLength = 1;
+    
   }
 
-  return longestStreak;
 
+
+  return longestLength;
 }
 
-let newSet = new Set()
-newSet.add(1);
-newSet.add(2);
-newSet.add(3);
-newSet.add(4);
-newSet.add(4);
-newSet.add(4);
-
-console.log(newSet)
-console.log(longestConsecutiveSequence([100, 4, 200, 1, 3, 2, 5, 8, 9]))
+console.log(longestConsecutiveSequence([100, 4, 200, 1, 3, 2, 5, 8, 9, 6]))
