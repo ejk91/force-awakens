9edf2b8d8822d2473f7c1707376bfcecdf052ada
Enoch Kim <enochkim91@gmail.com>
2017-10-03 21:46:06 -0700

    Completes RomanNumberTranslator

--- dumbledores-pensieve/romanNumberTranslator.js
+++ dumbledores-pensieve/romanNumberTranslator.js
@@ -16,17 +16,43 @@
  * You should return `null` on invalid input.
  */
 
-var DIGIT_VALUES = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
-};
+
 
 var translateRomanNumeral = function(romanNumeral) {
-// TODO: Implement me!
 
+    // I played before V or X indicates one less, so IV is four and IX is 9
+    // ex: XIV --> should be 14
+    const toInteger = {
+        I: 1,
+        V: 5,
+        X: 10, 
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    
+    // MCMXCVI
+    
+    let integer = 0; // 2000
+    let prev = null; // 100
+    
+    for (let i = 0; i < s.length; i++) {
+        // if value is greater than prev value and prev exists
+        const current = toInteger[s[i]];
+        if (prev && current > prev) {
+            // subtract prev from integer
+            integer -= prev;
+            // add current - prev to integer
+            integer += (current - prev);
+            prev = null;
+        } else {
+        // else 
+            // add current value
+            integer += current;
+            prev = current;
+        }
+    }
+    
+    return integer;
 };
